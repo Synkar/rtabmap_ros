dec1a7c6bbba76dd583f96e150151ac86fa5171c
Nigel Dias <nigeldias2012@gmail.com>
2026-08-14 15:42:34 -0400

    Fix: building with OpenCV 4.5.5 with a standard Ubuntu base image

--- docker/noetic/superpoint_openvino/ros_entrypoint.sh
+++ docker/noetic/superpoint_openvino/ros_entrypoint.sh
@@ -4,17 +4,9 @@ set -e
 # Preserve CMD args: OpenVINO setupvars.sh (and some ROS hooks) can clobber $@.
 _CMD_ARGS=("$@")
 
-# OpenVINO environment (official openvino/ubuntu20_* images)
-if [ -f /opt/intel/openvino/setupvars.sh ]; then
-  # shellcheck disable=SC1091
-  source /opt/intel/openvino/setupvars.sh
-elif [ -f /opt/intel/openvino_2024/setupvars.sh ]; then
-  # shellcheck disable=SC1091
-  source /opt/intel/openvino_2024/setupvars.sh
-elif ls /opt/intel/openvino_*/setupvars.sh >/dev/null 2>&1; then
-  # shellcheck disable=SC1090
-  source "$(ls -1 /opt/intel/openvino_*/setupvars.sh | head -n1)"
-fi
+# OpenVINO environment + Synkar OpenCV dir
+# shellcheck disable=SC1091
+source /etc/synkar/build_env.sh
 
 # setup ros environment
 # shellcheck disable=SC1091
